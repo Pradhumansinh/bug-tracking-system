cc7a37b93a826a69ff324e191f417f12ad7252d0
Pradhumansinh <vaghelapradhumansinh09@gmail.com>
2026-04-30 23:43:24 +0530

    Update AppRoutes.jsx

--- MERN/Frontend/src/routes/AppRoutes.jsx
+++ MERN/Frontend/src/routes/AppRoutes.jsx
@@ -1,24 +1,17 @@
 import react from "react"
 import {Routes,Route} from "react-router-dom"
-
 import Login from "../components/Login.jsx"
 import Register from "../components/Register.jsx"
-
 import MainDashboard from "../components/MainDashboard.jsx"
-
 import Bugs from "../components/Bugs.jsx"
-
 import Layout from "../components/Layout.jsx"
 import ProtectedRoute from "./ProtectedRoutes.jsx"
-
 import CreateBug from "../components/CreateBug.jsx"
 import Projects from "../components/Project.jsx"
 import ProjectDashboard from "../components/ProjectDashboard.jsx"
 import UserManagement from "../components/UserManagement.jsx"
-
 import ForgotPassword from "../components/ForgotPassword.jsx"
 import ResetPassword from '../components/ResetPassword.jsx'
-
 import Profile from "../components/Profile.jsx"
 import Setting from "../components/Setting.jsx"
 import TeamOverview from "../components/TeamOverview.jsx"
@@ -28,7 +21,7 @@ const AppRoutes = ()=>{
         <Routes>
             <Route path = "/" element = {<Login/>}/>
             <Route path = "/register" element = {<Register/>}/>
-               <Route path="/profile" element={<ProtectedRoute><Layout><Profile /></Layout></ProtectedRoute>} />
+            <Route path="/profile" element={<ProtectedRoute><Layout><Profile /></Layout></ProtectedRoute>} />
             <Route path ="/dashboard" element={<ProtectedRoute><Layout><MainDashboard /></Layout></ProtectedRoute>} />
             <Route path = "/bugs" element = {<ProtectedRoute><Layout><Bugs /></Layout></ProtectedRoute>}/>
             <Route path = "/create-bug" element={<ProtectedRoute><Layout><CreateBug /></Layout></ProtectedRoute>} />
